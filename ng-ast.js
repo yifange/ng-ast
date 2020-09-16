fe39b22517a8e696a25098b38f5e78edc95c7a71
Yifan Ge <yifange@google.com>
2020-09-15 22:43:18 -0700

    Update ng-ast.js
    Add edgeCount and toDot

--- ng-ast.js
+++ ng-ast.js
@@ -18,7 +18,15 @@
 
   function moduleToNode(name) {
     if (!name) {
-      return null;
+      return {
+        name: 'UNKNOWN',
+        dependencies: [],
+        values: [],
+        constants: [],
+        services: [],
+        factories: [],
+        children: [],
+      };
       // throw new Error('Expected angular module name');
     }
     if (_modules[name]) {
@@ -77,5 +85,88 @@
     return moduleToNode(name);
   }
 
+  function edgeCount() {
+    ngAst('buganizer').then((x) => {
+      const m = {};
+
+      const parseTree = (root) => {
+        if (!root) {
+          return;
+        }
+
+        const name = toShortName(root.name);
+
+        if (m[name]) {
+          m[name]['in'] += 1;
+        } else {
+          m[name] = {out: root.children.length, in: 1};
+        }
+
+        for (let child of root.children) {
+          parseTree(child);
+        }
+      };
+
+      parseTree(x);
+
+      const s = [];
+      for (const [name, entry] of Object.entries(m)) {
+        s.push(`${name}\t${entry.in}\t${entry.out}`);
+      }
+
+      console.log(s.join('\n'));
+    });
+  }
+
+  function toShortName(name) {
+    if (name === 'buganizer') {
+      return name
+    } else if (name.startsWith('buganizer.features')) {
+      return name.slice(19);
+    } else if (name.startsWith('buganizer')) {
+      return name.slice(10);
+    } else if (name.startsWith('google3.devtools.buganizer.client')) {
+      return name.slice(37);
+    } else if (name.startsWith('google3/devtools/buganizer/client')) {
+      return name.slice(37).replaceAll('/', '.');
+    } else {
+      return name;
+    }
+  }
+
+  function sanitizeName(name) {
+    return name.replaceAll('.', '_');
+  }
+
+  function toDot() {
+    const prelude = 'digraph {\n  graph [stylesheet="https://g3doc.corp.google.com/frameworks/g3doc/includes/graphviz-style.css"]\n node [shape=box, style=rounded]\n';
+    const epilogue = '}';
+    ngAst('buganizer').then((x) => {
+      const lines = [];
+
+      const parseTree = (root) => {
+        if (!root || root.children.length === 0) {
+          return;
+        }
+
+        const name = sanitizeName(toShortName(root.name));
+        const childrenNames = root.children.map((c) => sanitizeName(toShortName(c.name)));
+
+        lines.push(`  ${name} -> {${childrenNames.join(', ')}}`);
+
+        for (const child of root.children) {
+          parseTree(child);
+        }
+      };
+
+      parseTree(x);
+
+      console.log(`${prelude}${lines.join('\n')}${epilogue}`);
+    });
+  }
+
   root.ngAst = ngAst;
+  root.toDot = toDot;
+  root.edgeCount = edgeCount;
+
 }(typeof window === 'object' ? window : this));
